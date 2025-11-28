abb4fea0f1e40e5ba860f31c4a994bf1ff366c7d
Bennycopter <ben@natural20design.com>
2025-11-27 19:34:17 -0500

    CIJ-v4

---
+++ prod/define/vidstack-player.js
@@ -0,0 +1,20 @@
+import { defineCustomElement } from '../chunks/vidstack-BGSTndAW.js';
+import { MediaPlayerElement, MediaProviderElement } from '../chunks/vidstack-DU_pcTpo.js';
+import '../chunks/vidstack-CnF8W1vH.js';
+import '../chunks/vidstack-DJDnh4xT.js';
+import '../chunks/vidstack-xMS8dnYq.js';
+import '../chunks/vidstack-ChQTHmIQ.js';
+import '../chunks/vidstack-DdDZyQQA.js';
+import '../chunks/vidstack-CTojmhKq.js';
+import '../chunks/vidstack-CydS4m5K.js';
+import '../chunks/vidstack-DYbwIVLq.js';
+import '../chunks/vidstack-D5EzK014.js';
+import '../chunks/vidstack-B01xzxC4.js';
+import '../chunks/vidstack-C9vIqaYT.js';
+import '../chunks/vidstack-C2US-gSO.js';
+import '@floating-ui/dom';
+import '../chunks/vidstack-Dihypf8P.js';
+import '../chunks/vidstack-DsPOyKtl.js';
+
+defineCustomElement(MediaPlayerElement);
+defineCustomElement(MediaProviderElement);
